18618adc93a99c044517deac3775f062f2801ba1
carla-zdn <zeidan@et.esiea.fr>
2020-05-08 22:50:27 +0200

    [develop] Small change

--- app/src/main/java/com/example/whereparty/ConcertApi.java
+++ app/src/main/java/com/example/whereparty/ConcertApi.java
@@ -5,5 +5,5 @@ import retrofit2.http.GET;
 
 public interface ConcertApi {
     @GET("api/3.0/metro_areas/28909/calendar.json?apikey=f7BntzROcFk22SBa")
-Call<RestConcertResponse> getConcertResponse();
+    Call<RestConcertResponse> getConcertResponse();
 }
