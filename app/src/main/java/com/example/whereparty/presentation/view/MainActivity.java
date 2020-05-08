ab18591ade08e1ea97933cf5f3e89b76c6238e60
carla-zdn <zeidan@et.esiea.fr>
2020-05-09 00:42:43 +0200

    [feature][architecture] End of the MVC architecture

--- app/src/main/java/com/example/whereparty/presentation/view/MainActivity.java
+++ app/src/main/java/com/example/whereparty/presentation/view/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 import com.example.whereparty.Constants;
 import com.example.whereparty.R;
 import com.example.whereparty.data.ConcertApi;
+import com.example.whereparty.presentation.controller.MainController;
 import com.example.whereparty.presentation.model.Event;
 import com.example.whereparty.presentation.model.RestConcertResponse;
 import com.google.gson.Gson;
@@ -32,43 +33,24 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ListAdapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
-    private SharedPreferences sharedPreferences;
-    private Gson gson;
+
+
+    private MainController controller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sharedPreferences = getSharedPreferences("cache_concert", Context.MODE_PRIVATE);
-
-        gson = new GsonBuilder()
-                .setLenient()
-                .create();
-
-        List<Event> eventList = getDataFromCache();
-
-        if(eventList != null){
-            showList(eventList);
-        }else{
-            makeApiCall();
-        }
+        controller = new MainController(getSharedPreferences("cache_concert", Context.MODE_PRIVATE),
+                new GsonBuilder()
+                        .setLenient()
+                        .create(), this
+        );
+        controller.onStart();
     }
 
-    private List<Event> getDataFromCache() {
-
-        String jsonEventList = sharedPreferences.getString(Constants.KEY_EVENT_LIST, null);
-
-        if(jsonEventList == null){
-            return null;
-        }else{
-            Type eventListType = new TypeToken<List<Event>>(){}.getType();
-            return gson.fromJson(jsonEventList, eventListType);
-        }
-
-    }
-
-    private void showList(List<Event> eventList) {
+    public void showList(List<Event> eventList) {
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
         // use a linear layout manager
@@ -80,46 +62,7 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(mAdapter);
     }
 
-    private void makeApiCall(){
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(Constants.BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .build();
-
-        ConcertApi concertApi = retrofit.create(ConcertApi.class);
-
-        Call<RestConcertResponse> call = concertApi.getConcertResponse();
-        call.enqueue(new Callback<RestConcertResponse>() {
-            @Override
-            public void onResponse(Call<RestConcertResponse> call, Response<RestConcertResponse> response) {
-                if(response.isSuccessful() && response.body() != null){
-                    List<Event> eventList = response.body().getResultPage().getResults().getEvent();
-                    saveList(eventList);
-                    showList(eventList);
-                }else{
-                    showError();
-                }
-            }
-
-            @Override
-            public void onFailure(Call<RestConcertResponse> call, Throwable t) {
-                showError();
-            }
-        });
-    }
-
-    private void saveList(List<Event> eventList) {
-
-        String jsonEventList = gson.toJson(eventList);
-
-        sharedPreferences
-                .edit()
-                .putInt("cle_integer", 3)
-                .putString(Constants.KEY_EVENT_LIST, jsonEventList)
-                .apply();
-    }
-
-    private void showError() {
+    public void showError() {
         Toast.makeText(this, "No data in cache", Toast.LENGTH_SHORT).show();
     }
 }
